c0f28209c633641edee78d9d0e3ca2c1b4424def
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-09-18 23:45:59 +0600

    First commit

---
+++ FormatSpecifiersInC.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  
+  //...format specifiers//
+  %c  //	Character
+  %d  //  Signed integer
+  %hi // signed integer [short]
+  %hu // unsigned integer [short]
+  %e or %E //Scientific notations , floats
+  %f //strictly floats
+  %lli or %lld  //prints long long
+  %li //prints long  
+  %llu //unsigned long long
+  %lu //unsigned long
+  %Lf //long double
+  %lf //double
+  %p //pointer
+  %s //string
+  %u //unsigned int
+  %x or %X //hexadecimal representation
+  %% //prints % character [bruh..!]
+  %i //unsigned integer
+  
+    
+    
+    
+    return 0;
+  
+}
