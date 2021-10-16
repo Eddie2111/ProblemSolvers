5dee0539ab34f97ab2fe1a88902fe32c651fd960
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-10-16 20:06:19 +0600

    Django Tutorial for beginers

---
+++ Django Tutorial.cs
@@ -0,0 +1,11 @@
+to create django project:
+
+python -m django startproject <projectName> .
+
+to start django project
+
+python manage.py runserver <port number>
+
+
+<projectName> - your projectname can be anything. Dot after project name does not creates extra folders.
+<port number> - your port number should be allocated from 3000-4000 for avoiding conflict
