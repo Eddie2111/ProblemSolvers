4a45b44085ce2b3e7e5e4951101475801ac07a87
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-10-20 15:10:22 +0600

    Create React Problems.cs

---
+++ React Problems.cs
@@ -0,0 +1,7 @@
+Sometimes windows cmd doesnot allow you to install or create a react app
+basically windows is allowing powershell to do so.
+
+To pass the barrier type
+set-executionpolicy remotesigned
+
+this will temporarily allow your powershell to control further
