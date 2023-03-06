684e9b130e0b4012021020e282440475eb71e3ef
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-03-06 23:12:40 +0600

    Update Django Tutorial.cs

--- Django Tutorial.cs
+++ Django Tutorial.cs
@@ -7,6 +7,5 @@ to start django project
 
 python manage.py runserver <port number>
 
-
 <projectName> - your projectname can be anything. Dot after project name does not creates extra folders.
 <port number> - your port number should be allocated from 3000-4000 for avoiding conflict
